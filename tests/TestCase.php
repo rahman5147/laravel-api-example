b9aed43e0a16d373252ec67a3e66512e026253a9
Rahman <rahmanromli@yahoo.com>
2016-09-09 18:34:59 +0800

    add test for save fruit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,4 +22,21 @@ abstract class TestCase extends Illuminate\Foundation\Testing\TestCase
 
         return $app;
     }
+
+    /**
+     * Return request headers needed to interact with the API.
+     *
+     * @return Array array of headers.
+     */
+    protected function headers($user = null){
+        $headers = ['Accept' => 'application/json'];
+
+        if (!is_null($user)) {
+            $token = JWTAuth::fromUser($user);
+            JWTAuth::setToken($token);
+            $headers['Authorization'] = 'Bearer '.$token;
+        }
+
+        return $headers;
+    }
 }
