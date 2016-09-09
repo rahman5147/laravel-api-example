54ca87792c9b0de3fd261cd466ebd6dd895b4130
Rahman <rahmanromli@yahoo.com>
2016-09-09 17:12:05 +0800

    add fruit model & update test

--- tests/FruitsTest.php
+++ tests/FruitsTest.php
@@ -8,6 +8,18 @@ class FruitsTest extends TestCase
 {
 	use DatabaseMigrations;
 
+	public function testFruit(){
+		$this->seed('FruitsTableSeeder');
+		$this->get('/api/fruits')
+         ->seeJsonStructure([
+             'data' => [
+                '*' => [
+                    'name', 'color', 'weight', 'delicious'
+                ]
+             ]
+         ]);
+	}
+
     public function testJSONFront(){
         $this->get('/api')
             ->seeJson([
