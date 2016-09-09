f69a96a7b7166605a9850d67f03671ccd5611961
Rahman <rahmanromli@yahoo.com>
2016-09-09 18:33:21 +0800

    add route for using jwt auth

--- routes/dingo.php
+++ routes/dingo.php
@@ -12,6 +12,10 @@ $api->version('v1', function ($api) {
 
     $api->get('fruit/{id}', 'App\Http\Controllers\FruitsController@show');
 
+    $api->post('authenticate', 'App\Http\Controllers\AuthenticateController@authenticate');
+	$api->post('logout', 'App\Http\Controllers\AuthenticateController@logout');
+	$api->get('token', 'App\Http\Controllers\AuthenticateController@getToken');
+
 	$api->get('test', function () {
         return 'It is ok';
     });
