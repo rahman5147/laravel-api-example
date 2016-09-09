dd060c36cb634252b1bf996ff7463d3363ec63ed
Rahman <rahmanromli@yahoo.com>
2016-09-09 17:38:34 +0800

    add test to fruit & comment api route

--- tests/FruitsTest.php
+++ tests/FruitsTest.php
@@ -8,22 +8,49 @@ class FruitsTest extends TestCase
 {
 	use DatabaseMigrations;
 
-	public function testFruit(){
-		$this->seed('FruitsTableSeeder');
-		$this->get('/api/fruits')
-         ->seeJsonStructure([
-             'data' => [
-                '*' => [
-                    'name', 'color', 'weight', 'delicious'
-                ]
-             ]
-         ]);
-	}
-
+	/**
+	* @test
+	* Test: GET /api.
+	*/
     public function testJSONFront(){
         $this->get('/api')
             ->seeJson([
                 'Fruits' => 'Delicious and healthy!'
         ]);
     }
+
+	/**
+	* @test
+	* Test: GET /api/fruits.
+	*/
+	public function testFruit(){
+		$this->seed('FruitsTableSeeder');
+		$this->get('/api/fruits')
+	        ->seeJsonStructure([
+	            'data' => [
+	                '*' => [
+	                    'name', 'color', 'weight', 'delicious'
+	                ]
+	            ]
+        ]);
+	}
+
+	/**
+	 * @test
+	 * Test: GET /api/fruit/1.
+	 */
+	public function it_fetches_a_single_fruit(){
+	    $this->seed('FruitsTableSeeder');
+
+	    $this->get('/api/fruit/1')
+	        ->seeJson([
+	            'data' => [
+	                'id'        => 1,
+	                'name'      => "Apple",
+	                'color'     => "Green",
+	                'weight'    => "150 grams",
+	                'delicious' => true
+	            ]
+        ]);
+	}
 }
