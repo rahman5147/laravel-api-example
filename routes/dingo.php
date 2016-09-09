1b9f808d7f8586665966b8140ddc2f9a2527cad1
Rahman <rahmanromli@yahoo.com>
2016-09-09 17:14:28 +0800

    add fruit route to dingo

--- routes/dingo.php
+++ routes/dingo.php
@@ -7,7 +7,9 @@ $api->version('v1', function ($api) {
 	$api->get('/', function() {
         return ['Fruits' => 'Delicious and healthy!'];
     });
-    
+
+    $api->get('fruits', 'App\Http\Controllers\FruitsController@index');
+
 	$api->get('test', function () {
         return 'It is ok';
     });
